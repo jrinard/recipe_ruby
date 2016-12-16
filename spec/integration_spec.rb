5fb5cdf088280b8ac2b743c819f2f52833c2da68
joshua <jrinard@hotmail.com>
2016-12-15 18:09:44 -0800

    reworked integrated spec

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -2,3 +2,63 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+
+describe('Recipe Route', {:type => :feature}) do
+  describe('Create Tag') do
+    it "allows user to create a tag" do
+      visit('/')
+      fill_in('tag', :with => "Snack")
+      click_button('Create Tag')
+      visit('/tags')
+      expect(page).to have_content 'Snack'
+    end
+    it "allows user to create a recipe" do
+      visit('/')
+      fill_in('new-recipe-name', :with => "pizza")
+      fill_in('new-ingedients', :with => "cheese")
+      fill_in('new-instructions', :with => "cook")
+      fill_in('new-rating', :with => "5.1")
+      fill_in('tag', :with => "Cookie")
+      click_button('Create Recipe')
+      visit('/recipes')
+      expect(page).to have_content 'Recipe List'
+    end
+  end
+  # 
+  # describe('Modify a recipe', {:type => :feature}) do
+  #   # it "allows user to update a recipe" do
+  #   #   recipe = Recipe.create({:name => "Pizza"})
+  #   #   visit("/recipes/#{recipe.id}")
+  #   #   fill_in('new-name', :with => "Pepperoni")
+  #   #   click_button("Update")
+  #   #   expect(page).to have_content 'Pepperoni'
+  #   # end
+  #   it "allows user to delete a recipe" do
+  #     recipe = Recipe.create({:name => "Pizza"})
+  #     visit("/recipes/#{recipe.id}")
+  #     click_button("Delete Recipe")
+  #     expect(page).not_to have_content 'Pizza'
+  #   end
+  # end
+
+  describe('Modify a Tag', {:type => :feature}) do
+    it "allows user to update a tag" do
+      tag = Tag.create({:name => "Dinner"})
+      visit("/tags/#{tag.id}")
+      fill_in('new_tag', :with => "Lunch")
+      click_button("Save")
+      expect(page).to have_content 'Lunch'
+    end
+    it "allows user to delete a tag" do
+      tag = Tag.create({:name => "Dinner"})
+      visit("/tags/#{tag.id}")
+      click_button("Delete")
+      expect(page).not_to have_content 'Dinner'
+    end
+  end
+
+
+
+
+end
