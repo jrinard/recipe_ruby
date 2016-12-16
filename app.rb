8970d4edcca5e7c09bfc3c39038e7d8e4f3ebc58
joshua <jrinard@hotmail.com>
2016-12-15 17:05:36 -0800

    Comment Refining

--- app.rb
+++ app.rb
@@ -11,6 +11,7 @@ after do
   ActiveRecord::Base.clear_active_connections!
 end
 
+
 #home route
 get('/') do
   @tags = Tag.all() #grabs all tags and makes them available
@@ -31,17 +32,19 @@ get('/recipe/:id') do
   erb(:recipe_detail)
 end
 
-#create cecipe
+#create recipe
 post('/recipes') do
   new_name = params['recipe-name'] #[] are the same as .fetch but defult to nil or NULL
   new_ingedients = params['ingedients']
   new_instructions = params['instructions']
   new_rating = params['rating']
   new_tag = Tag.find(params['tag_id'].to_i)
+  #creates new recipe with data it fetched above
   @recipes = Recipe.create({:name => new_name, :ingredients => new_ingedients, :instructions => new_instructions, :ratings => new_rating})
+  #pushes new tag to the recipe you just created
   @recipes.tags.push(new_tag)
   if @recipes.save
-    redirect "/"
+  redirect "/recipes"
   else
     erb(:errors)
   end
@@ -53,9 +56,13 @@ patch('/recipe/:id') do
   new_recipe_ingedients = params["new-ingedients"]
   new_recipe_instructions = params["new-instructions"]
   new_recipe_rating = params["new-rating"]
+  #finds the recipe that matches the id
   @recipe = Recipe.find(params["id"].to_i)
+  #update the recipe with the data fetched above
   @recipe.update({:name => new_recipe_name, :ingredients => new_recipe_ingedients, :instructions => new_recipe_instructions, :ratings => new_recipe_rating})
+  #destroys all tags associated with recipe
   @recipe.tags.destroy_all
+  #pushes new tag based on the id that was fetched above to the recipe
   @recipe.tags.push(Tag.find(params["tag_id"]))
   redirect '/recipes'
 end
@@ -72,37 +79,40 @@ end
 #create tag
 post('/tags') do
   new_tag = params['tag']
-  @tag = Tag.new({:name  => new_tag})
-  if @tag.save
-    redirect "/"
-  else
-    erb(:errors)
-  end
+  #create a new tag with the value in the form and name it @tag
+  @tag = Tag.create({:name  => new_tag})
+  redirect "/"
 end
 
 #tags - multiple
 get('/tags') do
+  #get all your tags
   @tags = Tag.all()
   erb(:tags)
 end
 
 #tags - single
 get('/tags/:id') do
+  #find the specific tag by id
   @tags = Tag.find(params['id'].to_i)
   erb(:tag)
+end
+
+
+#update tag
+patch('/tags/:id') do
+  #grab the new tag name
+  new_tag_name = params['new_tag']
+  #find the tag with the  id
+  @tags = Tag.find(params['id'].to_i)
+  #update that found tag with the new name
+  @tags.update({:name => new_tag_name})
+  @tags = Tag.all()
+  erb(:tags)
 end
 
 #delete tag
 delete('/tags/:id') do
   Tag.find(params['id'].to_i).destroy
   redirect '/tags'
-end
-
-#update tag
-patch('/tags/:id') do
-  new_tag_name = params['new_tag']
-  @tags = Tag.find(params['id'].to_i)
-  @tags.update({:name => new_tag_name})
-  @tags = Tag.all()
-  erb(:tags)
 end
